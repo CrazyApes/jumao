a212ae0b20076f841deb65ead6f2c38e896dd602
zhangxun <429380443@qq.com>
2017-05-25 19:32:02 +0800

    定义了价格配置参数表

---
+++ src/main/java/cn/com/crazyit/core/constant/PriceConfType.java
@@ -0,0 +1,9 @@
+package cn.com.crazyit.core.constant;
+
+/**
+ * @author Zack
+ *         Created on 2017/5/24.
+ */
+public enum PriceConfType {
+    WHOLE_PACKAGE_DOOR,DOUBLE_SLEEVE,SINGLE_SLEEVE
+}
---
+++ src/main/java/cn/com/crazyit/foundation/pojo/PriceConf.java
@@ -0,0 +1,70 @@
+package cn.com.crazyit.foundation.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * @author Zack
+ *         Created on 2017/5/24.
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@Entity
+@Table(name = "PRICE_CONF")
+public class PriceConf extends ApplicationPojo{
+    //父配制id
+    @Column(updatable = false)
+    private Long parentConfId;
+
+    // 所属客户
+    @ManyToOne(targetEntity = Customer.class)
+    @JoinColumn(name = "CUSTOMER_ID", nullable = false)
+    private Customer customer;
+
+    //高度超出加的价格
+    @Column(nullable = false)
+    private Long incrementPriceForH;
+
+    //宽度超出加的价格
+    @Column(nullable = false)
+    private Long incrementPriceForW;
+
+    //深度度超出加的价格
+    @Column(nullable = false)
+    private Long incrementPriceForD;
+
+    /*//增加一种颜色加的价格
+    @Column(nullable = false)
+    private Long incrementPricePerColor;*/
+
+    //200以内基础价格
+    @Column(nullable = false)
+    private Long basePriceA;
+
+    //300以内基础价格
+    @Column(nullable = false)
+    private Long basePriceB;
+
+    @Enumerated
+    @Column(length = 20, nullable = false)
+    private String priceConfType;
+
+    //基础宽度
+    @Column(nullable = false)
+    private Integer baseHeight;
+
+    //基础宽度
+    @Column(nullable = false)
+    private Integer baseWidth;
+
+    //基础深度
+    @Column(nullable = false)
+    private Integer baseDepth;
+
+
+
+}
